c6a380c536569fcd714212d3098daced4ee7368d
iron-cherep <jacobxblue@gmail.com>
2017-03-05 18:37:41 +0300

    изменил логику переключения вкладок

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
 window.onload = function() {
-  init.itemHoverEvent();
+  init.itemClickEvent();
 }
 
-var info = ".page-main__info",
-    listItem = ".page-main__list-item",
+var $listItem = $(".page-main__list-item"),
+    $itemButton = $(".project-button"),
+    info = ".page-main__info",
     init = {
 
+      itemHoverEvent: function() {
+        $listItem.hoverIntent(function() {
+          $(this).find(info).slideToggle(600);
+        });
+      },
 
-  itemHoverEvent: function() {
-    $(listItem).hoverIntent(function() {
-      $(this).find(info).slideToggle(600);
-    });
-  }
+      itemClickEvent: function() {
 
-}
+        $itemButton.on("click", function() {
+
+          var $thisItem = $(this).siblings(info),
+              allItems = $itemButton.siblings(info);
+
+          if (!$thisItem.hasClass("active")) {
+            allItems.removeClass("active").slideUp(600);
+            $thisItem.addClass("active").slideDown(600);
+          } else {
+            allItems.removeClass("active").slideUp(600);
+          }
+        });
+
+      }
+
+    }
